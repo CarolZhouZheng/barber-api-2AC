7e75f2d35a3210728d93107cf8b4de6ae1a1ba45
Carol Zhou <carolinazhouzheng05@gmail.com>
2026-04-26 18:39:12 -0600

    todo merga

--- src/main/java/com/una/barberia/BarberiaApplication.java
+++ src/main/java/com/una/barberia/BarberiaApplication.java
@@ -5,4 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BarberiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarberiaApplication.class, args);
     }
+}
